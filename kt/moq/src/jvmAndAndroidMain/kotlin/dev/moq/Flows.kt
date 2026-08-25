f452776bcf8dd3548410efecef5361766293fd9a
Luke Curley <kixelated@gmail.com>
2026-08-24 17:54:33 -0700

    fix(bindings): hide compatibility documentation

--- kt/moq/src/jvmAndAndroidMain/kotlin/dev/moq/Flows.kt
+++ kt/moq/src/jvmAndAndroidMain/kotlin/dev/moq/Flows.kt
@@ -178,6 +178,7 @@ fun MoqTrackDynamic.requestedGroups(): Flow<MoqGroupRequest> = flow {
     if (cause is CancellationException) cancel()
 }
 
+/** @suppress */
 @Deprecated("dynamic routing is not currently supported by clients")
 fun MoqOriginDynamic.requestedBroadcasts(): Flow<MoqBroadcastRequest> = flow {
     while (true) {
