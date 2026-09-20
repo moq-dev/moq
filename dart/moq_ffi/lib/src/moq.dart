0b1a332fc16ae39c604e98d9ff8efa30f06671bb
Luke Curley <kixelated@gmail.com>
2026-09-20 14:34:12 -0700

    fix(ffi): refresh dart API checksum

--- dart/moq_ffi/lib/src/moq.dart
+++ dart/moq_ffi/lib/src/moq.dart
@@ -8,9 +8,7 @@ import "dart:ffi";
 import "dart:io" show Platform, File, Directory;
 import "dart:isolate";
 import "dart:typed_data";
-
 import "package:ffi/ffi.dart";
-
 import "uniffi_runtime.dart";
 export "uniffi_runtime.dart";
 
@@ -11955,7 +11953,7 @@ void _checkApiChecksums() {
     throw UniffiInternalError.panicked("UniFFI API checksum mismatch");
   }
   if (uniffi_moq_ffi_checksum_method_moqbroadcastproducer_set_catalog_section() !=
-      25735) {
+      28423) {
     throw UniffiInternalError.panicked("UniFFI API checksum mismatch");
   }
   if (uniffi_moq_ffi_checksum_method_moqbroadcastproducer_set_video_properties() !=
